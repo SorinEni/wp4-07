9e9464ee8cdee79822663892378a49c839b48174
SorinEni <sorin.eni@student.ossp.cz>
2022-03-05 11:12:47 +0100

    authentificator

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,19 +5,24 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use App\Model\PostFacade;
+use App\Model\UserFacade;
 use Nette;
+use Nette\Application\UI\Form;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
 	private PostFacade $facade;
+	private UserFacade $userFacade;
 
-	public function __construct(PostFacade $facade)
+	public function __construct(PostFacade $facade, UserFacade $userFacade)
 	{
 		$this->facade = $facade;
+		$this->userFacade = $userFacade;
 	}
 
 	public function renderDefault(): void
 	{
+
 		$this->template->posts = $this->facade
 			->getPublicArticles()
 			->limit(5);
